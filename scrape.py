44f2ffbd3299052d1e8257531046bac7287b12df
David Zmick <dpzmick@gmail.com>
2014-03-18 21:30:40 -0500

    scrape

--- scrape.py
+++ scrape.py
@@ -1,2 +1,14 @@
-from lxml import html
-import requests
+import urllib2
+from BeautifulSoup import BeautifulSoup
+import json
+
+# get the web page
+soup = BeautifulSoup(urllib2.urlopen('https://ankiweb.net/shared/addons/').read())
+
+# get the javascript data
+jdata = str(soup.find('div', {'id': 'content'}).findAll('script')[1])
+jdata = jdata[24:-29] # gross!! TODO seriously?
+
+for addon in json.loads(jdata):
+    addon_id = addon[0]
+    addon_name = addon[1]
