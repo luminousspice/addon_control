663328b8852e2d8fad8a1b85fb77365fae10296a
danish21 <daninoorani21@gmail.com>
2014-03-18 21:32:04 -0500

    fuzzy

---
+++ addonD.py
@@ -0,0 +1,17 @@
+
+
+class addonD: 
+	
+
+	def __init__(self, name1, title1, codeNumber1):
+		self.name=name1
+		self.title=title1
+		self.codeNumber=codeNumber1
+		print(self.name, self.title, self.codeNumber)
+
+	
+
+
+
+
+
