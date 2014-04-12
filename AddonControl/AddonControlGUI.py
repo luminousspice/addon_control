eb6b169f2de3a279a167343667f2e5b28c924fad
David Zmick <dpzmick@gmail.com>
2014-04-12 17:11:11 -0500

    layout and widgets added, now need to do some actions

--- AddonControl/AddonControlGUI.py
+++ AddonControl/AddonControlGUI.py
@@ -9,5 +9,49 @@ class AddonControlGUI(QtGui.QWidget):
 
     def initUI(self):
         self.setWindowTitle('AddonControl')
+
+        # main layout
+        self.vbox = QtGui.QVBoxLayout()
+        self.vbox.addStretch(1)
+
+        self.setup_filter_row()
+        self.setup_table_row()
+        self.setup_buttons_row()
+
+        self.setLayout(self.vbox)
+
         self.show()
+
+    def setup_filter_row(self):
+        filter_field = QtGui.QLineEdit(self)
+        filter_button = QtGui.QPushButton('Filter', self)
+
+        hbox = QtGui.QHBoxLayout()
+        hbox.addStretch(1)
+        hbox.addWidget(filter_field)
+        hbox.addWidget(filter_button)
+
+        self.vbox.addLayout(hbox)
+
+    def setup_table_row(self):
+        table = QtGui.QTableWidget(self)
+
+        hbox = QtGui.QHBoxLayout()
+        hbox.addStretch(1)
+        hbox.addWidget(table)
+
+        self.vbox.addLayout(hbox)
+
+    def setup_buttons_row(self):
+        install_button = QtGui.QPushButton('Install', self)
+        #update_button = QtGui.QPushButton('Update', self)
+        remove_button = QtGui.QPushButton('Remove', self)
+
+        hbox = QtGui.QHBoxLayout()
+        hbox.addStretch(1)
+        hbox.addWidget(install_button)
+        hbox.addWidget(remove_button)
+
+        self.vbox.addLayout(hbox)
+
 
